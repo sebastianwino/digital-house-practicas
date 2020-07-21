33f6af6c6f50d3d04c2e71b408dc4e5044db1336
sebastianwino <swinog@hotmail.com>
2020-07-20 22:26:34 -0300

    2020/07/20

---
+++ JS-front/DOM/practicaDOM/src/controllers/mainControllers.js
@@ -0,0 +1,7 @@
+let mainControllers = {
+    index: (req, res) => {
+        res.render('index')
+    }
+}
+
+module.exports = mainControllers
---
+++ JS-front/DOM/practicaDOM/src/routes/mainRoutes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const mainControllers = require('../controllers/mainControllers')
+
+/* GET home page. */
+router.get('/', mainControllers.index);
+
+module.exports = router;
